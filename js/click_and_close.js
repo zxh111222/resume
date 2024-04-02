f34ca154a9fca2d494025b7a7fcb9302a8c2ce7f
zxhhhxz1221 <1305894626@qq.com>
2024-04-03 00:21:58 +0800

    update hidden part

---
+++ js/click_and_close.js
@@ -0,0 +1,25 @@
+function closeContent() {
+  // 将内容区域隐藏起来
+  document.getElementById("content").classList.remove("visible");
+  document.getElementById("content").classList.add("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // 点击图标时触发的函数
+  document.getElementById("icon").addEventListener("click", function() {
+    // 隐藏图标
+    document.getElementById("icon").style.display = "none";
+    document.getElementById("text1").style.display = "none";
+    // 将内容区域显示出来
+    document.getElementById("content").classList.remove("hidden");
+    document.getElementById("content").classList.add("visible");
+  });
+
+  // 点击关闭按钮时触发的函数
+  document.querySelector("#content .close").addEventListener("click", function() {
+    closeContent();
+    // 显示图标
+    document.getElementById("icon").style.display = "block";
+    document.getElementById("text1").style.display = "block";
+  });
+});

174844b341d3f8db6c7f9b051e3a416f7a6161f6
zxhhhxz1221 <1305894626@qq.com>
2024-04-04 18:27:16 +0800

    update 4.4

--- js/click_and_close.js
+++ js/click_and_close.js
@@ -1,7 +1,7 @@
 function closeContent() {
   // 将内容区域隐藏起来
-  document.getElementById("content").classList.remove("visible");
-  document.getElementById("content").classList.add("hidden");
+  document.getElementById("hidden-content").classList.remove("visible");
+  document.getElementById("hidden-content").classList.add("hidden");
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("icon").style.display = "none";
     document.getElementById("text1").style.display = "none";
     // 将内容区域显示出来
-    document.getElementById("content").classList.remove("hidden");
-    document.getElementById("content").classList.add("visible");
+    document.getElementById("hidden-content").classList.remove("hidden");
+    document.getElementById("hidden-content").classList.add("visible");
   });
 
   // 点击关闭按钮时触发的函数
-  document.querySelector("#content .close").addEventListener("click", function() {
+  document.querySelector("#hidden-content .close").addEventListener("click", function() {
     closeContent();
     // 显示图标
     document.getElementById("icon").style.display = "block";
